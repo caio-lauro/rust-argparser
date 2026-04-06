b103777d60a5ae40e823c2072b9361b6d25cfcb1
caio-lauro <caiolaurodelima@gmail.com>
2026-04-06 14:20:23 -0300

    add basic example of usage

---
+++ examples/basic.rs
@@ -0,0 +1,35 @@
+use argparser::{Argument, ArgumentParser, ArgumentType::*, OptionalArgument, ParsedValue};
+
+fn main() {
+    let parser = ArgumentParser::new()
+        .add_arg(Argument::from("input", Text))
+        .add_arg(Argument::from("output", Text))
+        .add_arg(OptionalArgument::from(
+            "verbose",
+            Some("v"),
+            Boolean,
+            ParsedValue::Boolean(false),
+        ))
+        .add_arg(OptionalArgument::from(
+            "count",
+            Some("c"),
+            Integer,
+            ParsedValue::Integer(1),
+        ));
+
+    let parsed = parser.parse(std::env::args()).unwrap_or_else(|e| {
+        eprintln!("error: {e}");
+        std::process::exit(1);
+    });
+
+    let input = parsed.get_value::<String>("input");
+    let output = parsed.get_value::<String>("output");
+    let verbose = parsed.get_value::<bool>("verbose");
+    let count = parsed.get_value::<i64>("count");
+
+    if verbose {
+        println!("input:  {input}");
+        println!("output: {output}");
+        println!("count:  {count}");
+    }
+}
